11322919a484928fead663dcd4cd00f751c25293
Avijit Das <avijit111187@gmail.com>
2020-01-01 23:50:38 +0530

    Function in python

---
+++ api/FunctionwithinFunction.py
@@ -0,0 +1,21 @@
+def hello(name='Jose'):
+    print('Hello Function has been Run') 
+    
+    def greet():
+        return "within greet"
+     
+    print(greet())
+    
+hello()
+#greet()
+
+def hello_world():
+  name = str(input("Enter your name: "))
+  if name == 'AV':
+    print ("Hello " + str(name))
+  else:
+    ret = "My World"
+  return ret
+  
+ret1 = hello_world()
+print (ret1)
